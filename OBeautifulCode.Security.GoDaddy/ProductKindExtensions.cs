226a64de7ec250e75f2cf755b3ae473690d17de4
Suraj Gupta <SurajGupta@users.noreply.github.com>
2019-03-03 12:57:58 -0500

    seed SimpleGoDaddyClient

---
+++ OBeautifulCode.Security.GoDaddy/ProductKindExtensions.cs
@@ -0,0 +1,58 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProductKindExtensions.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Security.GoDaddy
+{
+    using System;
+    using OBeautifulCode.Validation.Recipes;
+
+    /// <summary>
+    /// Extension methods on <see cref="ProductKind"/>.
+    /// </summary>
+    public static class ProductKindExtensions
+    {
+        /// <summary>
+        /// Converts a certificate product kind to a string representation
+        /// that can be used with the GoDaddy API.
+        /// </summary>
+        /// <param name="productKind">The kind of certificate product.</param>
+        /// <returns>
+        /// A string representation of the specified certificate product kind
+        /// that can be used with the GoDaddy API.
+        /// </returns>
+        public static string ToApiCompatibleString(
+            this ProductKind productKind)
+        {
+            new { productKind }.Must().NotBeEqualTo(ProductKind.Unknown);
+
+            switch (productKind)
+            {
+                case ProductKind.DomainValidationSsl:
+                    return "DV_SSL";
+                case ProductKind.DomainValidationWildcardSsl:
+                    return "DV_WILDCARD_SSL";
+                case ProductKind.ExtendedValidationSsl:
+                    return "EV_SSL";
+                case ProductKind.OrganizationValidationCodeSigning:
+                    return "OV_CS";
+                case ProductKind.OrganizationValidationDriverSigning:
+                    return "OV_DS";
+                case ProductKind.OrganizationValidationSsl:
+                    return "OV_SSL";
+                case ProductKind.OrganizationValidationWildcardSsl:
+                    return "OV_WILDCARD_SSL";
+                case ProductKind.UnifiedCommunicationDomainValidationSsl:
+                    return "UCC_DV_SSL";
+                case ProductKind.UnifiedCommunicationExtendedValidationSsl:
+                    return "UCC_EV_SSL";
+                case ProductKind.UnifiedCommunicationOrganizationValidationSsl:
+                    return "UCC_OV_SSL";
+            }
+
+            throw new NotSupportedException("This certificate product kind is not supported: " + productKind);
+        }
+    }
+}
---
+++ OBeautifulCode.Security.GoDaddy/SimpleGoDaddyClient.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SimpleGoDaddyClient.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Security.GoDaddy
+{
+    using System.Threading.Tasks;
+
+    using OBeautifulCode.Validation.Recipes;
+
+    /// <summary>
+    /// A <see cref="GoDaddyClient"/> wrapper that simplifies it's usage.
+    /// </summary>
+    public class SimpleGoDaddyClient
+    {
+        private readonly GoDaddyClient client;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleGoDaddyClient"/> class.
+        /// </summary>
+        /// <param name="accessKey">The API access key.</param>
+        /// <param name="secretKey">The API secret key.</param>
+        /// <param name="environment">The GoDaddy environment to connect to.</param>
+        public SimpleGoDaddyClient(
+            string accessKey,
+            string secretKey,
+            GoDaddyEnvironment environment)
+        {
+            this.client = new GoDaddyClient(accessKey, secretKey, environment);
+        }
+
+        /// <summary>
+        /// Create a new certificate.
+        /// </summary>
+        /// <param name="pemEncodedCsr">A PEM-encoded CSR.</param>
+        /// <param name="numberOfYearsCertIsValidFor">The number of years that the certificate is valid for.</param>
+        /// <param name="productKind">The kind of certificate product.</param>
+        /// <returns>
+        /// The certificate identifier.
+        /// </returns>
+        public async Task<string> CreateNewCertAsync(
+            string pemEncodedCsr,
+            int numberOfYearsCertIsValidFor,
+            ProductKind productKind)
+        {
+            new { pemEncodedCsr }.Must().NotBeNullNorWhiteSpace();
+            new { numberOfYearsCertIsValidFor }.Must().BeGreaterThanOrEqualTo(1).And().BeLessThanOrEqualTo(3);
+            new { productKind }.Must().NotBeEqualTo(ProductKind.Unknown);
+
+            var request = new CertificateCreate
+            {
+                Csr = pemEncodedCsr,
+                Period = numberOfYearsCertIsValidFor,
+                ProductType = productKind.ToApiCompatibleString(),
+            };
+
+            var response = await this.client.CreatePendingOrderForCertificateAsync(request);
+
+            var result = response.CertificateId;
+
+            return result;
+        }
+    }
+}
